0938e72788166e5605deac09f3fc3fd2089d36e6
SamuelJames101 <SamuelJames101101@gmail.com>
2023-08-16 13:37:47 +0100

    Inital commit

---
+++ EditorKeyboardIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EditorKeyboardIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ EditorKeyboardIssue/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace EditorKeyboardIssue;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+	}
+}
+
+
